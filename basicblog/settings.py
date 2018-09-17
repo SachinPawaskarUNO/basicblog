dd631a2e5787b552ff2f10d5b5a365031c45c1f9
Sachin Pawaskar <spawaskar@unomaha.edu>
2018-09-17 09:48:26 -0500

    changes for heroku deploy

--- basicblog/settings.py
+++ basicblog/settings.py
@@ -30,19 +30,6 @@ ALLOWED_HOSTS = ['*']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd5n98qer3v6lvd',
-        'USER': 'hguadziuufmcxp',
-        'PASSWORD': 'ed0fdf25323af42fefeddecc291aaa7d50b3ea2406391a287d55e22f21f59365',
-        'HOST': 'ec2-54-83-27-165.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
-
 try:
     from .local_settings import *
 except ImportError:
