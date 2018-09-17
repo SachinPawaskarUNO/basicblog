d74a06a163a7e1a900e75474305bfff1a0815d46
Sachin Pawaskar <spawaskar@unomaha.edu>
2018-09-17 09:37:22 -0500

    changes for heroku deploy

--- basicblog/settings.py
+++ basicblog/settings.py
@@ -139,6 +139,7 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
